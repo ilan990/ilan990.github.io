7e9f2525645f9ab9755b9f64cc0b1ec6f6caefbd
ilan990 <ilan2.assouline@gmail.com>
2021-12-20 22:05:51 +0100

    Réglage de l'horloge  avec ajout du 0 quand c'est inferieur à 10

--- JS/EcranAccueil.js
+++ JS/EcranAccueil.js
@@ -2,9 +2,17 @@ $(function(){
 let hours;
 setInterval(function() {
   var d = new Date();
-  hours = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  var hours=d.getHours();
+  var minutes=d.getMinutes();
+  if (hours<10){
+    hours="0"+hours;
+  }
+  if (minutes<10){
+    minutes="0"+minutes;
+  }
+  hours = hours + ":" + minutes ;
   console.log(hours);
-  $(".title1").text(hours)
+  $(".heure").text(hours)
 }, 1000);
 
  
