51910defbf9305a233e1e4e55cbabcaa1fa4a8cf
ilan990 <ilan2.assouline@gmail.com>
2021-12-25 23:35:44 +0100

    ajout du design mot de passe

--- JS/EcranAccueil.js
+++ JS/EcranAccueil.js
@@ -1,4 +1,6 @@
 $(function(){
+
+//Affichage de l'heure
 let hours;
 setInterval(function() {
   var d = new Date();
@@ -11,9 +13,22 @@ setInterval(function() {
     minutes="0"+minutes;
   }
   hours = hours + ":" + minutes ;
-  console.log(hours);
   $(".heure").text(hours)
 }, 1000);
 
+
+//points pour le mot de passe
+for (let i=0;i<4;i++){
+  $(".points").append("<div class='point point"+i+"'></div>")
+}
  
+
+
+//Numéros pour le mot de passe
+for (let i=1;i<10;i++){
+  $(".numbers").append("<div class='number number"+i+"'>"+i+"</div>")
+}
+  $(".numbers").append("<div></div>")
+  $(".numbers").append("<div class='number number0'>0</div>")
+
 })
