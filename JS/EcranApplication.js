161e27c8bced957cc52575a13fe882a11170dd1b
ilan990 <ilan2.assouline@gmail.com>
2022-01-22 22:28:16 +0100

    Suppression des vh dans les applications

--- JS/EcranApplication.js
+++ JS/EcranApplication.js
@@ -1,5 +1,15 @@
 $(function(){
-
+let frames=["frameMain","frameAccueil","frameEcranApplication","frameApplicationProjets"];
+let titleApplicationTop=["Projets","autre"]
+function framevisible(nbr){
+  for(let i=0;i<frames.length;i++){
+    if (i!=nbr){
+      $("."+frames[i]).css("display","none");
+    }else{
+      $("."+frames[i]).css("display","flex");
+    }
+  }
+}
 //Affichage de l'heure
 setInterval(function() {
   var d = new Date();
@@ -16,11 +26,15 @@ setInterval(function() {
 }, 1000);
 
 //Affichage des applications
-for (let i=1;i<16;i++){
-  $(".applicationsTop").append("<div class='application'><div class='application"+i+"'></div><div class='applicationTitle"+i+"'></div></div>")
+for (let i=3;i<5;i++){
+  $(".applicationsTop").append("<div class='application' id="+i+"><div class='application"+i+"'></div><div class='applicationTitle"+i+"'>"+titleApplicationTop[i-3]+"</div></div>")
 }
 for (let i=1;i<5;i++){
   $(".applicationsBottom").append("<div class='applicationBottom'><div class='applicationB"+i+"'></div><div class='applicationBTitle"+i+"'></div></div>")
 }
+
+$(".application").click(function(){
+  framevisible($(this).attr('id'));
+})
 
 })
