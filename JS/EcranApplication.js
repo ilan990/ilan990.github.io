550175a196d2f161ec3882ee59668d6107f79b4e
ilan990 <ilan2.assouline@gmail.com>
2022-01-23 22:53:12 +0100

    Mise à jour 23 janvier 2022, ajout de la partie projets

--- JS/EcranApplication.js
+++ JS/EcranApplication.js
@@ -1,6 +1,7 @@
 $(function(){
 let frames=["frameMain","frameAccueil","frameEcranApplication","frameApplicationProjets"];
-let titleApplicationTop=["Projets","autre"]
+let titleApplicationTop=["Projets","CV"]
+let imgApplication=["https://img.icons8.com/color/48/000000/ms-edge-new.png","https://img.icons8.com/material-rounded/48/000000/file--v2.png"]
 function framevisible(nbr){
   for(let i=0;i<frames.length;i++){
     if (i!=nbr){
@@ -27,7 +28,7 @@ setInterval(function() {
 
 //Affichage des applications
 for (let i=3;i<5;i++){
-  $(".applicationsTop").append("<div class='application' id="+i+"><div class='application"+i+"'></div><div class='applicationTitle"+i+"'>"+titleApplicationTop[i-3]+"</div></div>")
+  $(".applicationsTop").append("<div class='application' id="+i+"><div class='application"+i+"'><img src='"+imgApplication[i-3]+"'/></div><div class='applicationTitle"+i+"'>"+titleApplicationTop[i-3]+"</div></div>")
 }
 for (let i=1;i<5;i++){
   $(".applicationsBottom").append("<div class='applicationBottom'><div class='applicationB"+i+"'></div><div class='applicationBTitle"+i+"'></div></div>")
