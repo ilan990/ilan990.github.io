a42250ced55186a6e7c5cf2b531740ec882e5ea5
ilan990 <ilan2.assouline@gmail.com>
2022-02-01 22:14:13 +0100

    Mise à jour de la page projets

--- JS/EcranApplication.js
+++ JS/EcranApplication.js
@@ -1,7 +1,7 @@
 $(function(){
 let frames=["frameMain","frameAccueil","frameEcranApplication","frameApplicationProjets"];
-let titleApplicationTop=["Projets","CV"]
-let imgApplication=["https://img.icons8.com/color/48/000000/ms-edge-new.png","https://img.icons8.com/material-rounded/48/000000/file--v2.png"]
+let titleApplicationTop=[["Projets","CV"],["Profil"]]
+let imgApplication=[["https://img.icons8.com/color/48/000000/ms-edge-new.png","img/Applications/pencil.png"],["img/Applications/profil.png"]]
 function framevisible(nbr){
   for(let i=0;i<frames.length;i++){
     if (i!=nbr){
@@ -31,10 +31,10 @@ setInterval(function() {
 
 //Affichage des applications
 for (let i=3;i<5;i++){
-  $(".applicationsTop").append("<div class='application' id="+i+"><div class='application"+i+"'><img src='"+imgApplication[i-3]+"'/></div><div class='applicationTitle"+i+"'>"+titleApplicationTop[i-3]+"</div></div>")
+  $(".applicationsTop").append("<div class='application' id="+i+"><div class='application"+i+"'><img src='"+imgApplication[0][i-3]+"'/></div><div class='applicationTitle"+i+"'>"+titleApplicationTop[0][i-3]+"</div></div>")
 }
-for (let i=1;i<5;i++){
-  $(".applicationsBottom").append("<div class='applicationBottom'><div class='applicationB"+i+"'></div><div class='applicationBTitle"+i+"'></div></div>")
+for (let i=1;i<2;i++){
+  $(".applicationsBottom").append("<div class='applicationBottom'><div class='applicationB"+i+"'><img src='"+imgApplication[1][i-1]+"'/></div><div class='applicationBTitle"+i+"'>"+titleApplicationTop[1][i-1]+"</div></div>")
 }
 
 $(".application").click(function(){
--- JS/EcranProjets.js
+++ JS/EcranProjets.js
@@ -17,4 +17,5 @@ $(".fermerProjets").click(function(){
 
 
 
+
 })
